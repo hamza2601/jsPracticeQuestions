a863fa6db39b8f9ddb5a724f0c9e84923a14ba98
Hamza Ali <98526515+hamza2601@users.noreply.github.com>
2023-09-13 12:01:55 +0500

    Create codeSignalAddProblem.js

---
+++ codeSignalAddProblem.js
@@ -0,0 +1,10 @@
+//Write a function that returns the sum of two numbers.
+
+function solution(param1, param2) {
+    let a = (-1000<=param1 && -1000 <= param2);
+    let b = (1000 >= param1 && 1000>= param2)
+    if (a && b){
+        let c = param1 + param2;
+        return c;
+    }
+}
